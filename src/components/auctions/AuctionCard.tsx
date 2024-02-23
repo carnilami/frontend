1b92dd080df7b16672d73319f3f13492a1670674
subhanyousaf <subhanyousaf2003@gmail.com>
2024-02-24 03:04:59 +0500

    :zap: Make whole card cliackable.

--- src/components/auctions/AuctionCard.tsx
+++ src/components/auctions/AuctionCard.tsx
@@ -43,7 +43,9 @@ const AuctionCard = ({
   expiry,
   price,
 }: AuctionCardProps) => {
-  const [currentUnixTimestamp, setCurrentUnixTimestamp] = useState<number>(moment().unix());
+  const [currentUnixTimestamp, setCurrentUnixTimestamp] = useState<number>(
+    moment().unix()
+  );
 
   const REFRESH_INTERVAL = 1000;
   useEffect(() => {
@@ -54,66 +56,71 @@ const AuctionCard = ({
     return () => clearInterval(intervalId);
   }, []);
 
-  const auctionRemainingTime = formatAuctionTimeRemaining(expiry, currentUnixTimestamp);
+  const auctionRemainingTime = formatAuctionTimeRemaining(
+    expiry,
+    currentUnixTimestamp
+  );
 
   return (
-    <ChakraBox
-      id={_id}
-      whileHover={{ scale: 1.02 }}
-      whileTap={{ scale: 0.95 }}
-      borderRadius={5}
-      overflow="hidden"
-    >
-      <Card overflow="hidden" variant="unstyled">
-        <CardBody>
-          <Box
-            position="relative"
-            display="inline-block"
-            w="100%"
-            borderRadius={5}
-            bg={"black"}
-          >
-            <Link to={`/auctions/${_id}`}>
-              <AspectRatio  ratio={3 / 2}>
-                <Image
-                  borderRadius={5}
-                  src={titleImage}
-                  objectFit="cover"
-                  w="100%"
-                  _hover={{ opacity: 0.8 }}
-                />
-              </AspectRatio>
-            </Link>
-            <Tag
-              position="absolute"
-              overflow="hidden"
-              bottom={0}
-              left={0}
-              backgroundColor="gray.900"
-              margin={2}
+    <Link to={`/auctions/${_id}`}>
+      <ChakraBox
+        id={_id}
+        whileHover={{ scale: 1.02 }}
+        whileTap={{ scale: 0.95 }}
+        borderRadius={5}
+        overflow="hidden"
+      >
+        <Card overflow="hidden" variant="unstyled">
+          <CardBody>
+            <Box
+              position="relative"
+              display="inline-block"
+              w="100%"
+              borderRadius={5}
+              bg={"black"}
             >
-              <Stack direction="row" spacing={1} alignItems={"center"}>
-                <TimeIcon color="gray" />
-                <Text color="white" fontWeight="700">
-                  {auctionRemainingTime}
-                </Text>
-                <Text color="gray">PKR</Text>
-                <Text color="white" fontWeight="700">
-                  {price.toLocaleString()}
-                </Text>
-              </Stack>
-            </Tag>
-          </Box>
-          <Heading size="sm" mt={2}>
-            <Link to={`/auctions/${_id}`}>{title}</Link>
-          </Heading>
-          <Text>
-            {price === 0 && <NoReserveBadge size="xs" />} {description}
-          </Text>
-          <Text color={"gray.400"}>{city}</Text>
-        </CardBody>
-      </Card>
-    </ChakraBox>
+              <Link to={`/auctions/${_id}`}>
+                <AspectRatio ratio={3 / 2}>
+                  <Image
+                    borderRadius={5}
+                    src={titleImage}
+                    objectFit="cover"
+                    w="100%"
+                    _hover={{ opacity: 0.8 }}
+                  />
+                </AspectRatio>
+              </Link>
+              <Tag
+                position="absolute"
+                overflow="hidden"
+                bottom={0}
+                left={0}
+                backgroundColor="gray.900"
+                margin={2}
+              >
+                <Stack direction="row" spacing={1} alignItems={"center"}>
+                  <TimeIcon color="gray" />
+                  <Text color="white" fontWeight="700">
+                    {auctionRemainingTime}
+                  </Text>
+                  <Text color="gray">PKR</Text>
+                  <Text color="white" fontWeight="700">
+                    {price.toLocaleString()}
+                  </Text>
+                </Stack>
+              </Tag>
+            </Box>
+            <Heading size="sm" mt={2}>
+              <Link to={`/auctions/${_id}`}>{title}</Link>
+            </Heading>
+            <Text>
+              {price === 0 && <NoReserveBadge size="xs" />} {description}
+            </Text>
+            <Text color={"gray.400"}>{city}</Text>
+          </CardBody>
+        </Card>
+      </ChakraBox>
+    </Link>
   );
 };
 
